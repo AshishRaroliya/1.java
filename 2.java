0c95b2d1548e1535b00e8e75ea64c18b4ba0af8c
ashish <ashishraroliya@integramicro.co.in>
2022-03-15 15:48:04 +0530

    srs

--- 2.java
+++ 2.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
 
-class Series 
-{
-    public static void test(int n )
-  {    int c =2;
-       int a =1;
-        System.out.print(a+",");
-       for(int i =1 ; i<=n-1 ; i++)
-    { 
-      a=a+c;
-      System.out.print(""+a+",");
-    }
- System.out.println("disco");
-  }
+class Series {
+	public static void test(int n) {
+		int c = 2;
+		int a = 1;
+		System.out.print(a + ",");
+		for (int i = 1; i <= n - 1; i++) {
+			a = a + c;
+			System.out.print("" + a + ",");
+		}
+		System.out.println("disco");
+	}
 
 }
 
-class Test
-{
-  public static void main(String[] args) 
+class Test {
+	public static void main(String[] args)
 
-  {   Scanner s = new Scanner(System.in);
-  
-      System.out.print("enter the length of series = ");
-      int n = s.nextInt();
-      Series s1 = new Series();
-             s1.test(n);
-  }
+	{
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("enter the length of series = ");
+		int n = s.nextInt();
+		Series s1 = new Series();
+		s1.test(n);
+	}
 }
